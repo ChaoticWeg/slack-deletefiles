2d516c2b1e8281da9c85439dc0626db0fcdd1050
ChaoticWeg <shawn@chaoticweg.cc>
2018-08-20 17:53:59 -0500

    implement

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from dotenv import load_dotenv
 from slack import get_files, delete_files
 
 def run():
-    get_files()
-    delete_files([])
+    while True:
+        try:
+            ids = get_files()
+            delete_files(ids)
+            if len(ids) < 1:
+                break
 
 if __name__ == "__main__":
     load_dotenv()
